3f1cd2f8fbacf1d003660e65ec3df4c36d17673a
tong <gaotongxiao@gmail.com>
2020-03-01 15:16:33 -0600

    Implement Chandy Lamport Protocol, fix bug in sending TODO: 1. ERR_SEND error 2. Blocking while executing atomic commands 3. program output

--- master.py
+++ master.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 import multiprocessing
 from multiprocessing import Queue, Process, Manager
 import threading
@@ -17,28 +17,74 @@ def observer(channels, lock, ):
             msg_type, msg = channels["m"]["o"].pop()
             if msg_type == "STOP":
                 break
+            elif msg_type == "BEGINSNAPSHOT":
+                node_id = int(msg[0])
+                channels["o"][node_id].appendleft(["SNAPSHOT", []])
+            elif msg_type == "COLLECTSTATE":
+                for recv_id, out_channel in channels["o"].items():
+                    if recv_id == "m":
+                        continue
+                    out_channel.appendleft(["COLLECT", []])
+                snapshot_state, snapshot_channels = {}, defaultdict(dict)
+                for send_id, _ in channels["o"].items():
+                    if send_id == "m":
+                        continue
+                    while len(channels[send_id]["o"]) <= 0:
+                        time.sleep(0.01)
+                    send_state, send_channel_state = channels[send_id]["o"].pop()
+                    snapshot_state[send_id] = send_state
+                    for in_id in send_channel_state.keys():
+                        if in_id == "m" or in_id == "o":
+                            continue
+                        snapshot_channels[in_id][send_id] = send_channel_state[in_id]
+                channels["o"]["m"].appendleft([snapshot_state, snapshot_channels])
             print("this is the msg_type {} with message {}".format(msg_type, msg))
 
 
 def member_node(channels, lock, node_id, init_money):
     money = init_money
     node_id = node_id
+    is_recording = {} 
+    num_channels_recording = 0
+    snapshot_money = []
+    snapshot_channels = {}
+    
     print("member_node set up!  node_id {}, money {}".format(node_id, money))
 
     while True:
-        if not channels["m"][node_id]:
+        if not (channels["m"][node_id]) and not (channels["o"][node_id]):
             time.sleep(0.1)
+        elif channels["o"][node_id]:
+            msg_type, msg = channels["o"][node_id].pop()
+            if msg_type == "SNAPSHOT":
+                snapshot_money = money
+                num_channels_recording = 0
+                for nb_id, out_channel in channels[node_id].items():
+                    if nb_id == 'm' or nb_id == 'o':
+                        continue
+                    is_recording[nb_id] = True
+                    out_channel.appendleft(['M', []]) # send Marker
+                    snapshot_channels[nb_id] = 0 # init snapshot memory
+                    num_channels_recording += 1
+            elif msg_type == "COLLECT":
+                num_channels_recording = 0
+                for nb_id, out_channel in channels[node_id].items():
+                    if nb_id == 'm' or nb_id == 'o':
+                        continue
+                    is_recording[nb_id] = False
+                channels[node_id]["o"].appendleft([snapshot_money, snapshot_channels])
         else:
             print("specific channel state is ", channels['m'][node_id])
             msg_type, msg = channels["m"][node_id].pop()
             if msg_type == "STOP":
                 break
-            if msg_type == "SEND":
+            elif msg_type == "SEND":
+                # TODO: ERR_SEND
                 recv_id, amount = msg[0], msg[1]
                 print("sending money.... to {}, with money {}".format(recv_id, amount))
+                money -= amount
                 channels[node_id][recv_id].appendleft(["X", [node_id, amount]])
-            
-            if msg_type == "RECV":
+            elif msg_type == "RECV":
                 send_id = msg[0]
                 if not channels[send_id][node_id]:
                     continue
@@ -47,9 +93,24 @@ def member_node(channels, lock, node_id, init_money):
                 lock.acquire()
                 msg_flag, msg = channels[send_id][node_id].pop()
                 if msg_flag == "M":
-                    pass #\TODO: special handling of marker msg
+                    if num_channels_recording == 0:
+                        snapshot_money = money
+                        for nb_id, out_channel in channels[node_id].items():
+                            if nb_id == 'm' or nb_id == 'o':
+                                continue
+                            is_recording[nb_id] = True
+                            out_channel.appendleft(['M', []]) # send Marker
+                            snapshot_channels[nb_id] = 0 # init snapshot memory
+                            num_channels_recording += 1
+                    else:
+                        if is_recording[send_id]:
+                            is_recording[send_id] = False
+                            num_channels_recording -= 1
+                        # otherwise ignore
                 else:
                     sender_id, amount = msg
+                    if is_recording.get(sender_id, False):
+                        snapshot_channels[sender_id] += amount
                     money += amount
                     print("receiving money ... from {}, with money {}".format(sender_id, amount))
                 
@@ -80,10 +141,6 @@ def receive_all_helper(channels, lock):
 
 
 
-    
-
-
-
 def main():
     #Set up
     thread_map = {}
@@ -98,6 +155,8 @@ def main():
         command = input()
         command_list = command.split()
         print("command recieved: ", command_list)
+        if len(command_list) == 0:
+            continue
         if command_list[0] == "StartMaster":
             lock.acquire()
             channels["m"] = {"o": deque()}
@@ -138,7 +197,7 @@ def main():
             channels["m"][int(src_id)].appendleft(["SEND", [int(dst_id), int(money)]])
         
         if command_list[0] == "Receive":
-            recv_id = command_list[1]
+            recv_id = int(command_list[1])
             if len(command_list) == 3:
                 send_id = command_list[2]
             else:
@@ -168,9 +227,24 @@ def main():
             print("finished!")
             break
 
+        if command_list[0] == "BeginSnapshot":
+            node_id = int(command_list[1])
+            channels["m"]["o"].appendleft(["BEGINSNAPSHOT", [node_id]])
+
+        if command_list[0] == "CollectState":
+            channels["m"]["o"].appendleft(["COLLECTSTATE", []])
+            while len(channels["o"]["m"]) <= 0:
+                continue
+            snapshot_state, snapshot_channels = channels["o"]["m"].pop()
+            print(snapshot_state)
+            print(snapshot_channels)
+
+        if command_list[0] == "q":
+            return
         else:
-            print("Unknown command, issue a new command!")
-            
+            continue
+            # print("Unknown command, issue a new command!")
+
 
 
     
